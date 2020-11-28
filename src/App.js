e56730beb2839bfc490601386e9b4d8c0c860393
Asbel Apaza <asbel.elmec@gmail.com>
2020-11-28 15:09:54 -0400

    ADD socket completed

--- src/App.js
+++ src/App.js
@@ -1,77 +1,96 @@
-import React, {useEffect, useState} from 'react';
-import io from 'socket.io-client';
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
 
 const { default: BandAdd } = require("./components/BandAdd");
 const { default: BandList } = require("./components/BandList");
 
 const connectSocketServer = () => {
-  const socket = io.connect('http://localhost:8080', {
-    transports: ['websocket']
+  const socket = io.connect("http://localhost:8080", {
+    transports: ["websocket"],
   });
   return socket;
-}
-
+};
 
 function App() {
-
   const [socket] = useState(connectSocketServer);
   const [online, setOnline] = useState(false);
   const [bands, setBands] = useState([]);
 
   useEffect(() => {
-    console.log(socket)
-    setOnline(socket.connected)
+    console.log(socket);
+    setOnline(socket.connected);
   }, [socket]);
 
   useEffect(() => {
-    socket.on('connect',() => {
+    socket.on("connect", () => {
       setOnline(true);
-    })
+    });
   }, [socket]);
-  
+
   useEffect(() => {
-    socket.on('disconnect',() => {
+    socket.on("disconnect", () => {
       setOnline(false);
-    })
+    });
   }, [socket]);
-  
+
   useEffect(() => {
-    socket.on('current-bands',(bands) => {
+    socket.on("current-bands", (bands) => {
       console.log(bands);
       setBands(bands);
-    })
+    });
   }, [socket]);
 
+  const votar = (id) => {
+    console.log("votar - app", id);
+    socket.emit("votar-banda", id);
+  };
+
+  const borrar = (id) => {
+    console.log("borrar - app", id);
+    socket.emit("borrar-banda", id);
+  };
+
+  const cambiarNombre = (id, nombre) => {
+    socket.emit("cambiar-nombre-banda", { id, nombre });
+  };
+
+  const crearBanda = ( nombre ) => {
+    socket.emit("crear-banda", {nombre} );
+  };
+
   return (
-    <div className="container" >
+    <div className="container">
       <div className="alert">
         <p>
           Service Status:
-          {
-            online
-              ? <span className="text-success" > Online </span>
-              : <span className="text-danger" > Offline </span>
-          }
-          
-          
+          {online ? (
+            <span className="text-success"> Online </span>
+          ) : (
+            <span className="text-danger"> Offline </span>
+          )}
         </p>
       </div>
 
       <h1>Band names</h1>
-      <hr/>
+      <hr />
 
       <div className="row">
         <div className="col-8">
           <BandList
             data={bands}
+            votar={votar}
+            borrar={borrar}
+            cambiarNombre={cambiarNombre}
           />
         </div>
 
         <div className="col-4">
-          <BandAdd/>
+          <BandAdd 
+            crearBanda ={crearBanda} 
+          
+          />
         </div>
       </div>
-
     </div>
   );
 }
--- src/components/BandAdd.js
+++ src/components/BandAdd.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const BandAdd = () => {
+const BandAdd = ({crearBanda}) => {
+
+    const [ valor, setValor ] = useState('')
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+
+        if (valor.trim().length > 0) {
+            crearBanda(valor);
+
+            setValor('')
+        }
+    }
+
     return (
         <>
             <h3> Agregar Banda </h3>
-            <form>
+            <form onSubmit={onSubmit}>
                 <input 
                 className="form-control"
                 placeholder="Nuevo nombre de Banda"
+                value={valor}
+                onChange= { (ev) => setValor(ev.target.value) }
                 />
             </form>
-            
+
         </>
     )
 }
